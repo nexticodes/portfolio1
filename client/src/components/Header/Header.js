fcf056ad31b99cdb51ff397f69fd06c766c16ed2
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-17 14:27:59 -0800

    Create Header component with EB Logo and collapsable button on the right side.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import logo from './../../assets/EBLogoWhite.png';
+
+import './Header.css';
+import DrawerButton from '../DrawerButton/DrawerButton';
+
 const Header = () => {
     return (
-        <div>
-
+        <div className='header__container'>
+            <div className='header__logo'>
+                <a href='/'><img src={logo} alt='EB Logo white' height='50px' width='50px'/></a>
+            </div>
+            <div className='spacer'></div>
+            <div className='header__toggle-button'>
+                <DrawerButton/>
+            </div>
         </div>
     )
 }
