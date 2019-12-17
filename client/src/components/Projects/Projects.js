e0c0d2fe7c83ab56fc5b2fbf0e4d10d3e5c14ab5
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-17 10:52:18 -0800

    Initial commit with some skeleton.

---
+++ client/src/components/Projects/Projects.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Projects = () => {
+
+    let projects = [
+        {
+            name: 'SweatDeckTM App',
+            link: 'https://fd-sweatdeck.herokuapp.com'
+        },
+        {
+            name: 'Optional Boss Website',
+            link: 'https://optional-boss.herokuapp.com'
+        }
+    ]
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Projects;
