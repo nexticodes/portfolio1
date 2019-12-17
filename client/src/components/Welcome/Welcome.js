c6933d74ca93277f3a9a26515f9772e4f341c814
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-17 15:34:55 -0800

    Edit styling of Welcome page. Add button that directs user down to another section / component.

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import './Welcome.css';
 
-const Welcome = () => {
+const Welcome = (props) => {
     return (
     <div className='welcome'>
         <div className='welcome__picture'>
             <div className='welcome__text'>
-                <h2>Hi, I'm Ernest Bruno</h2>
-                <h2>Front-End Developer</h2>
-                <button className='fancy-button'>Learn More About Me!</button>
+                <h2>Hi, I'm Ernest Bruno.</h2>
+                <h2>I am a Front-End Web Developer.</h2>
+                <button onClick={props.scroll} className='fancy-button'>Learn More About Me!</button>
             </div>
         </div>
     </div>
