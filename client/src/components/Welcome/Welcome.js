cca4d74d4f671499d5cea8a6a028c9a76e2bd2ef
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-17 14:29:06 -0800

    Fill in welcome page and import all the other components that I might need.

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import './Welcome.css';
+
 const Welcome = () => {
     return (
-    <div>
-
+    <div className='welcome'>
+        <div className='welcome__picture'>
+            <div className='welcome__text'>
+                <h2>Hi, I'm Ernest Bruno</h2>
+                <h2>Front-End Developer</h2>
+                <button>Learn More About Me!</button>
+            </div>
+        </div>
     </div>
     );
 };
