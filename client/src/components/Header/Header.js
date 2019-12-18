b570d3e313c95637e98f25c9c9a4998767b81a41
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-18 13:08:03 -0800

    Add conditional style.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,9 +5,15 @@ import logo from './../../assets/EBLogoWhite.png';
 import './Header.css';
 import DrawerButton from '../DrawerButton/DrawerButton';
 
-const Header = () => {
+const Header = (props) => {
+
+    let style = {
+        position: 'fixed',
+        animation: 'fade'
+    }
+
     return (
-        <div className='header__container'>
+        <div className='header__container' {...(props.currentSection !== 'welcome') ? style={style} : ''}>
             <div className='header__logo'>
                 <a href='/'><img src={logo} alt='EB Logo white' height='50px' width='50px'/></a>
             </div>
