90c43aed17608541d8760df8d35cc5fdd919e1bb
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-18 13:05:00 -0800

    Create Skills component that contain progress bars.

---
+++ client/src/components/Skills/Skills.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import './Skills.css';
+
+import ProgressBar from '../ProgressBar/ProgressBar';
+
+const Skills = () => {
+
+    const stats = [
+        {
+            name: 'HTML',
+            level: 80
+        },
+        {
+            name: 'CSS',
+            level: 60
+        },
+        {
+            name: 'JavaScript',
+            level: 75
+        },
+        {
+            name: 'React',
+            level: 80
+        },
+        {
+            name: 'Node.js',
+            level: 70
+        },
+        {
+            name: 'C#',
+            level: 50
+        },
+        {
+            name: 'UI/UX Design',
+            level: 35
+        }
+
+    ]
+    return (
+        <div className='skills__container'>
+            <div className='skill__names'>
+                {stats.map( (e,i) => <h3 keys={i}>{e.name}</h3> )}
+            </div>
+            <div className='skill__bars'>
+                {stats.map( (e,i) => {
+                return <div className='skill'><ProgressBar keys={i} percentage={e.level}/><h4>{e.level}%</h4></div>
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Skills;
