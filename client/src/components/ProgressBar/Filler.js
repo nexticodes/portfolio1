da4a99b6dfd1c3df824fc5aa7031f0a2a57d4960
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-18 13:03:17 -0800

    Create a component for progress bar.

---
+++ client/src/components/ProgressBar/Filler.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Filler.css';
+
+const Filler = (props) => {
+    return (
+        <div className='filler' style={{width: `${props.percentage}%`}}>
+
+        </div>
+    )
+}
+
+export default Filler;
