3f33caaa1a58605f95da9fb104497fa7b5454ee1
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-12-19 15:28:20 -0800

    Change size of logo.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,21 +1,18 @@
 import React from 'react';
 
-import logo from './../../assets/EBLogoWhite.png';
+import logo from './../../assets/EBLogoinv.png';
 
 import './Header.css';
 import DrawerButton from '../DrawerButton/DrawerButton';
 
 const Header = (props) => {
 
-    let style = {
-        position: 'fixed',
-        animation: 'fade'
-    }
+
 
     return (
-        <div className='header__container' {...(props.currentSection !== 'welcome') ? style={style} : ''}>
+        <div className='header__container' style={{'position': 'fixed'}}>
             <div className='header__logo'>
-                <a href='/'><img src={logo} alt='EB Logo white' height='50px' width='50px'/></a>
+                <a href='/'><img src={logo} alt='EB Logo white' height='75px' width='75px'/></a>
             </div>
             <div className='spacer'></div>
             <div className='header__toggle-button'>
