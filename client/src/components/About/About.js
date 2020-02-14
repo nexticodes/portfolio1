891ab7ceb9a8e27854b64fe217e7d7038e8fa8af
Ernest Bruno <ernest.bruno94@yahoo.com>
2020-02-13 17:33:14 -0800

    Put 'About Me' title on correct component.

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -14,7 +14,6 @@ const About = (props) => {
                     <img className='profilePic' src={pic}/>
                 </div>
                 <div className='about__banner'>
-                
                     <hr/>
                     <h4> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </h4>
                     <hr/>
